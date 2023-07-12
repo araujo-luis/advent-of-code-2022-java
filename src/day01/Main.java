9019c0fcdd88b88854f654cfdd58073cf07d949f
Luis Araujo <luis.araujo@viaplaygroup.com>
2023-07-12 11:28:52 +0200

    Day 1

--- src/day01/Main.java
+++ src/day01/Main.java
@@ -1,7 +1,7 @@
 package day01;
 
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -10,11 +10,23 @@ public class Main {
         File file = new File("/Users/luisarau/IdeaProjects/Java/src/day01/Part1.txt");
         BufferedReader br;
         String st;
+        PriorityQueue<Integer> caloriesByElf = new PriorityQueue<>(Collections.reverseOrder());
         try {
             br = new BufferedReader(new FileReader(file));
+            int counter = 0;
             while ((st = br.readLine()) != null) {
-                System.out.println(st);
+                if (st.equals("")) {
+                    caloriesByElf.add(counter);
+                    counter = 0;
+                    continue;
+                }
+                counter += Integer.parseInt(st);
             }
+
+            Integer part1 = caloriesByElf.poll();
+            System.out.println("Part 1: " + part1);
+            System.out.println("Part 2: " + (part1 + caloriesByElf.poll() + caloriesByElf.poll()));
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
